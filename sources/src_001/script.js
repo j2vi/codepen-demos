6bdf22e5420d3ddde4b774bd72209659bc171d25
j2vi <vitaly92a@gmail.com>
2021-12-07 12:24:01 +0300

    001 remastered

---
+++ sources/src_001/script.js
@@ -0,0 +1,38 @@
+/************************************************
+LIST OF RESERVED WORDS (JavaScript)
+
+// JavaScript Reserved Words //
+
+abstract;	arguments; await*;
+boolean; break;	byte;
+case;	catch; char; class*; const; continue;
+debugger;	default; delete; do; double;	
+else;	enum*; eval; export*;	extends*;	
+false;	final; finally;	float;	for;	function;
+goto;
+if;	implements;	import*; in; instanceof;	int;	interface;
+let*;	long;
+native;	new; null;
+package;	private;	protected; public;
+return;
+short;	static; super*;	switch;	synchronized;
+this; throw;	throws;	transient; true; try;	typeof;
+var;	void; volatile;
+while;	with;
+yield;
+
+Words marked with* are new in ECMAScript 5 and 6.
+*************************************************
+
+// Removed Reserved Words
+The following reserved words have been removed
+from the ECMAScript 5/6 standard//
+
+abstract	boolean	byte	char
+double	final	float	goto
+int	long	native	short
+synchronized	throws	transient	volatile
+
+
+Copypasted from W3School (https://www.w3schools.com/JS/js_reserved.asp)
+**********************************************************************/
